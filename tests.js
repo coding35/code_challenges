1f9f3e9e6ddd67878875f9b77338390bcd25535f
Adam Reed <adam.reed@keefegroup.com>
2019-08-31 09:05:13 -0500

    regex string validation

--- tests.js
+++ tests.js
@@ -225,3 +225,30 @@ QUnit.test('Return each word captialized. Regex.', function (assert) {
   assert.ok(Execute('i ran there') == 'I Ran There', 'Passed!');
 });
 
+
+/* Have the function SimpleSymbols(str) take the str parameter being passed and determine if it is an
+  acceptable sequence by either returning the string true or false. The str parameter will be composed
+  of + and = symbols with several characters between them (ie. ++d+===+c++==a) and for the string to be
+  true each letter must be surrounded by a + symbol. So the string to the left would be false.
+  The string will not be empty and will have at least one letter. 
+*/
+
+  QUnit.test('Return truthy if string validates.', function (assert) {
+
+  function Execute(input) {
+    let arr = input.split('');
+
+    let i = input.match(/^\+[a-z]+$/gi);
+
+    console.log(i);
+
+    // regex
+
+    
+
+
+  }
+
+  assert.ok(Execute('+d+=3=+s+') == true, 'Passed!');
+  assert.ok(Execute('f++d+') == false, 'Passed!');
+});
